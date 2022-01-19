74f99523203f9a24f66a36efe4b9ebc71ecc8e34
Chris-1965 <56934142+Chris-1965@users.noreply.github.com>
2022-01-19 14:07:04 +0100

    u

--- admin/words.js
+++ admin/words.js
@@ -27,55 +27,52 @@ systemDictionary = {
 		"zh-cn": "选项1"
 	},
 	"password": {
-		"en": "Password",
-		"de": "Passwort",
-		"ru": "вариант 2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
+		"en": "Passwort",
+		"de": "Kennwort",
+		"ru": "Пароль",
+		"pt": "Senha",
+		"nl": "wachtwoord",
+		"fr": "Mot de passe",
+		"it": "Password",
+		"es": "Contraseña",
+		"pl": "Hasło",
+		"zh-cn": "密码"
 	},
 	"refresh": {
 		"en": "Refresh rate",
-		"de": "Refresh Rate",
-		"ru": "вариант 2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
+		"de": "Aktualisierungsrate",
+		"ru": "Частота обновления",
+		"pt": "Taxa de atualização",
+		"nl": "Vernieuwingsfrequentie",
+		"fr": "Fréquence de rafraîchissement",
+		"it": "Frequenza di aggiornamento",
+		"es": "Frecuencia de actualización",
+		"pl": "Częstotliwość odświeżania",
+		"zh-cn": "刷新率"
 	},
 	"lightpath": {
 		"en": "Path to state for light switching",
-		"de": "State Pfad für Licht ein/aus",
+		"de": "Pfad zum Zustand für Lichtschaltung",
+		"ru": "Путь к состоянию для переключения света",
+		"pt": "Caminho para o estado para comutação de luz",
+		"nl": "Pad naar status voor lichtschakeling",
+		"fr": "Chemin d'accès à l'état pour la commutation de la lumière",
+		"it": "Percorso verso lo stato per la commutazione della luce",
+		"es": "Camino al estado para el cambio de luz",
+		"pl": "Ścieżka do stanu przełączania światła",
+		"zh-cn": "灯切换的状态路径"
 	},
 	"astrolight": {
 		"en": "Switch On light automatically with switch sauna with astro times",
-		"de": "Licht automatisch mit Sauna einschalten -> Astro Zeiten",
+		"de": "Licht automatisch einschalten mit Schalter Sauna mit Astrozeiten",
+		"ru": "Автоматическое включение света с помощью переключателя сауны с астрономическим временем",
+		"pt": "Acender a luz automaticamente com interruptor sauna com tempos astro",
+		"nl": "Schakel het licht automatisch in met schakelaar sauna met astrotijden",
+		"fr": "Allumer la lumière automatiquement avec interrupteur sauna avec horaires astro",
+		"it": "Accensione automatica della luce con interruttore sauna con orari astronomici",
+		"es": "Encienda la luz automáticamente con el interruptor de sauna con tiempos astronómicos",
+		"pl": "Automatyczne włączanie światła za pomocą przełącznika sauny z czasami astro",
+		"zh-cn": "使用带有天文时代的开关桑拿自动打开灯"
 	},
-	"huumStatus0": {
-		"en": "Sauna is offline",
-		"de": "Sauna ist Offline",
-	},
-	"huumStatus1": {
-		"en": "sauna online but not heating",
-		"de": "Sauna ist Online ohne heizen",
-	},
-	"huumStatus2": {
-		"en": "online and heating",
-		"de": "Online und heizt",
-	},
-	"huumStatus3": {
-		"en": "sauna is beeing used by another user and is locked",
-		"de": "Sauna wird von anderem User verwendet",
-	},
-	"huumStatus4": {
-		"en": "sauna is put to emergency stop",
-		"de": "Sauna Fehler -> Stop",
-	}
+
 };
