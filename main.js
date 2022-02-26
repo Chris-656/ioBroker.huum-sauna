ee442772457f50ed72b0740c34393ea0f379ba0d
Chris-1965 <56934142+Chris-1965@users.noreply.github.com>
2022-02-26 10:55:16 +0100

    FIX: Interval

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const utils = require("@iobroker/adapter-core");
 // Load your modules here, e.g.:
 const axios = require("axios").default;
 const axiosTimeout = 8000;
-const sleepRefresh = 120;							// Sleep Refresh in seconds when not on
+const sleepRefresh = 60*30;							// Sleep Refresh in seconds when sauna not switched on
 
 const sunCalc = require("suncalc2");               	// https://github.com/andiling/suncalc2
 
