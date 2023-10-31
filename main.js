33517000d24f1c838407ed3a7b76bfffcd2da562
Chris-1965 <Besterquester@live.at>
2023-10-31 07:28:26 +0100

    MAINT:Fix

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ class HuumSauna extends utils.Adapter {
 			this.refresh = this.config.sleep * 60;
 
 
-			this.log.info(`Logged in to HUUM User:${this.config.user}`);
+			this.log.info(`Logging in to HUUM User:${this.config.user}`);
 
 			this.getSaunaStatus()
 				.then(() => {
@@ -228,7 +228,6 @@ class HuumSauna extends utils.Adapter {
 	}
 
 	async getSaunaStatus() {
-
 		try {
 			const response = await axios.get(url, {
 				auth: {
@@ -238,22 +237,24 @@ class HuumSauna extends utils.Adapter {
 				timeout: axiosTimeout
 			});
 
-			if (response.data.statusCode === 231) {
+			if (response.data.statusCode == 231) {
 				this.syncAppValues(response.data);
 				await this.checkTempReached();
 				await this.checkSteamError();
 				//await this.checkSteamTemperatur();
 				this.log.info(`HUUM Request: statusCode: ${this.huum.statusCode} temperature:${this.huum.temperature} targetTemp:${this.huum.targetTemperature} sethumidity:${this.huum.humidity} Door closed:${this.huum.door} Config:${this.huum.config} light:${this.huum.light} steamerError:${this.huum.steamerError}  `);
-			} else if (response.data.statusCode === 232) {
+			} else if (response.data.statusCode == 232) {
 				this.syncAppValues(response.data);
 				this.log.info(`HUUM Request: statusCode: ${this.huum.statusCode} temperature:${this.huum.temperature}  sethumidity:${this.huum.humidity} Door closed:${this.huum.door} Config:${this.huum.config} light:${this.huum.light} steamerError:${this.huum.steamerError}  `);
 			} else {
-				this.log.warn(`Warning: Sauna Status: ${response.data.statusCode}`);
+				//this.log.warn(`Warning: Sauna Status: ${response.data.statusCode}`);
+				throw new Error(`Sauna not connected: ${response.data.statusCode}`);
 			}
 
 		} catch (error) {
 			this.huum = { "statusCode": 403 };
-			this.log.warn(`Warning: + ${error}`);
+			this.log.warn(`Warning Axios: + ${error}`);
+
 		}
 	}
 
